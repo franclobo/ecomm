8fcc6400a66d03d906de42b39b7957a7dda212a9
franclobo <fjbl2788@gmail.com>
2024-08-25 17:39:42 -0500

    Edit env variables

--- config/initializers/paypal.rb
+++ config/initializers/paypal.rb
@@ -1,6 +1,6 @@
 PayPal::Recurring.configure do |config|
   config.sandbox = true
-  config.username = ENV[PAYPAL_SANDBOX_USERNAME].to_s
-  config.password = ENV[PAYPAL_SANDBOX_PASSWORD].to_s
-  config.signature = ENV[PAYPAL_SANDBOX_SIGNATURE].to_s
+  config.username = ENV['PAYPAL_SANDBOX_USERNAME'].to_s
+  config.password = ENV['PAYPAL_SANDBOX_PASSWORD'].to_s
+  config.signature = ENV['PAYPAL_SANDBOX_SIGNATURE'].to_s
 end
